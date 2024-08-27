2304f3e8a8ad280bbb007bedd6e495e4c230d260
Cláudio Roberto <claudioroberto0707@outlook.com>
2024-08-26 22:33:33 -0300

    subindo projeto

---
+++ src/Pages/App.js
@@ -0,0 +1,14 @@
+import gitLogo from "../Assets/gitlogo.png";
+
+import {Container} from "./styles";
+
+function App() {
+  return (
+    <Container>
+      <img src={gitLogo} width={160} height={160} alt="Logo-Github" />
+    
+    </Container>
+  );
+}
+
+export default App;
